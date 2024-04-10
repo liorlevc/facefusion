93f7e66f7d2628a97b18e3e965c88e1f78c040c2
liorlevc <110098294+liorlevc@users.noreply.github.com>
2024-04-10 12:57:02 +0300

    Update wording.py

--- facefusion/wording.py
+++ facefusion/wording.py
@@ -123,7 +123,7 @@ WORDING : Dict[str, Any] =\
 		'stop_button': 'STOP',
 		'clear_button': 'CLEAR',
 		# about
-		'donate_button': 'DONATE',
+		'donate_button': 'DEMO MODE',
 		# benchmark
 		'benchmark_results_dataframe': 'BENCHMARK RESULTS',
 		# benchmark options
